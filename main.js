9b4e5767a7fc86ef9e2b32ec7308e0d78c04fda1
Greg Johnston <gregmj.sco@gmail.com>
2023-09-16 16:27:16 +0900

    Make LinkedList and MyNode classes with placeholder methods

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log("Test")
+
+class LinkedList{
+    constructor(Node) {
+
+    }
+    append(value) {}
+    prepend(value) {}
+    size() {}
+    head() {}
+    tail() {}
+    at(index) {}
+    pop() {}
+    contains(value) {}
+    find(value) {}
+    toString() {}
+
+}
+
+class myNode {
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+let misa = new myNode("Misa", null);
+
+console.log(misa.nextNode)
